2865ed4e1815b34248129df6eccad4d15295cf01
Brett Viren <brett.viren@gmail.com>
2019-07-27 10:43:21 -0400

    Just build in the test

--- testing/run.sh
+++ testing/run.sh
@@ -4,8 +4,8 @@ git clone --depth 1 https://github.com/brettviren/ptmp.git
 cd ptmp
 python3 ./tools/waf configure --prefix=$HOME
 python3 ./tools/waf --notests install
-python3 ./tools/waf --alltests -j1
-python3 ./tools/waf clean
+#python3 ./tools/waf --alltests -j1
+#python3 ./tools/waf clean
 
 
 
